4391be4e81585ab77f936a390191dcdb26f89545
namle10102003 <Namle10102003@gmail.com>
2025-11-05 23:45:42 +0700

    create Login Page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from "./pages/login/Login";
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,9 @@
+import "./Register.scss"
+
+const Register = () => {
+    return (
+        <div className="Register">Register</div>
+    )
+}
+
+export default Register
