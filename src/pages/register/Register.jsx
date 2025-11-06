a032c6e11ae936a1240fbcc8f12a0792502991d3
namle10102003 <Namle10102003@gmail.com>
2025-11-06 23:03:56 +0700

    create Register Page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,27 @@
-import "./Register.scss"
+import "./register.scss"
 
 const Register = () => {
     return (
-        <div className="Register">Register</div>
+        <div className="register">
+            <div className="card">
+                <div className="left">
+                    <h1>Lama Social.</h1>
+                    <p>Welcome to my application</p>
+                    <span>Do you have an account?</span>
+                    <button>Login</button>
+                </div>
+                <div className="right">
+                    <h1>Register</h1>
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <input type="text" placeholder="Name" />
+                        <button>Register</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 
