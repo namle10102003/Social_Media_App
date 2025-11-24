7ba18ffee5d44665966142f278e330d3dc620262
namle10102003 <Namle10102003@gmail.com>
2025-11-24 09:18:36 +0700

    Add Protected Route Logic for Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,61 @@
+import NavBar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar"
+import RightBar from "./components/rightBar/RightBar"
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Home from "./pages/home/Home"
+import Profile from "./pages/profile/Profile"
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
+  Outlet,
+  Navigate,
 } from "react-router-dom";
 
 function App() {
 
+  const currentUser = true;
+
   const Layout = () => {
-    return(
+    return (
       <div>
-        
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
       </div>
     )
   }
 
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+
+    return children
+  }
+
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ]
+    },
     {
       path: "/login",
       element: <Login />,
