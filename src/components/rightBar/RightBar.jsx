8be373c27cd6fa40d6ceb054d7396e99e82f6cf8
namle10102003 <Namle10102003@gmail.com>
2025-11-24 20:00:22 +0700

    Done Left Bar and Fix some Layout Errors

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -2,7 +2,152 @@ import "./rightBar.scss"
 
 const RightBar = () => {
     return (
-        <div className="rightBar">RightBar</div>
+        <div className="rightBar">
+            <div className="container">
+                <div className="item">
+                    <span>Suggestions For You</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                        <div className="buttons">
+                            <button>follow</button>
+                            <button>dismiss</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="item">
+                    <span>Latest Activities</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <p>
+                                <span>Laurent Garcia</span> changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <p>
+                                <span>Laurent Garcia</span> changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <p>
+                                <span>Laurent Garcia</span> changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <p>
+                                <span>Laurent Garcia</span> changed their cover picture
+                            </p>
+                        </div>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="item">
+                    <span>Online Friends</span>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                    <div className="user">
+                        <div className="userInfo">
+                            <img src="https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg" alt="" />
+                            <div className="online" />
+                            <span>Laurent Garcia</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
