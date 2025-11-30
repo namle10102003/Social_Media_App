370d174430b4eb2aa39df73b5d7cb43713a4ab26
namle10102003 <Namle10102003@gmail.com>
2025-11-30 08:55:16 +0700

    Done Comments Section

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,61 +1,7 @@
 import "./posts.scss";
-import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
-import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
-import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
-import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import { Link } from "react-router-dom";
-// import Comments from "../comments/Comments";
-import { useState, useContext } from "react";
+import Post from "../post/Post";
+import { useContext } from "react";
 import { AuthContext } from "../../context/authContext.jsx";
-
-const Post = ({ post }) => {
-    const [commentOpen, setCommentOpen] = useState(false);
-
-    //TEMPORARY
-    const liked = false;
-
-    return (
-        <div className="post">
-            <div className="container">
-                <div className="user">
-                    <div className="userInfo">
-                        <img src={post.profilePic} alt="" />
-                        <div className="details">
-                            <Link
-                                to={`/profile/${post.userId}`}
-                                style={{ textDecoration: "none", color: "inherit" }}
-                            >
-                                <span className="name">{post.name}</span>
-                            </Link>
-                            <span className="date">1 min ago</span>
-                        </div>
-                    </div>
-                    <MoreHorizIcon />
-                </div>
-                <div className="content">
-                    <p>{post.desc}</p>
-                    <img src={post.img} alt="" />
-                </div>
-                <div className="info">
-                    <div className="item">
-                        {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-                        12 Likes
-                    </div>
-                    <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
-                        <TextsmsOutlinedIcon />
-                        12 Comments
-                    </div>
-                    <div className="item">
-                        <ShareOutlinedIcon />
-                        Share
-                    </div>
-                </div>
-                {commentOpen && <Comments />}
-            </div>
-        </div>
-    );
-};
 
 const Posts = () => {
     const { currentUser } = useContext(AuthContext);
@@ -64,8 +10,16 @@ const Posts = () => {
         {
             id: 1,
             userId: 1,
-            name: currentUser?.name || "Someone",
-            profilePic: currentUser?.profilePic || "https://via.placeholder.com/40",
+            name: currentUser?.name || "Laurent Garcia",
+            profilePic: currentUser?.profilePic || "https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg",
+            desc: "This is a sample post",
+            img: "https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg",
+        },
+        {
+            id: 2,
+            userId: 2,
+            name: currentUser?.name || "Laurent Garcia",
+            profilePic: currentUser?.profilePic || "https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg",
             desc: "This is a sample post",
             img: "https://images.pexels.com/photos/27690887/pexels-photo-27690887.jpeg",
         },
